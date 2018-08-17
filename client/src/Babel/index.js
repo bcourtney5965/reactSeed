6ff1c6454bbc4764265b91234d431189ec647a34
Brandon Courtney <brandon.h.courtney@gmail.com>
2018-08-16 23:23:34 -0700

    Builds out the rest of the SASS directories and files

--- client/src/Babel/index.js
+++ client/src/Babel/index.js
@@ -216,6 +216,20 @@ export default class Babel extends Component {
         <div className={this.exponentiation() ? '' : 'babel-error'}>Exponentiation operator works: {this.exponentiation().toString()}</div>
         <div className={this.state.asyncAwaitWorks ? '' : 'babel-error'}>Async await: check console for AsyncAwait errors</div>
         <div className={this.objectValues() ? '' : 'babel-error'}>Object.values works: {this.objectValues().toString()}</div>
+        <div>
+          <h1>CSS3 Grid</h1>
+          <div className="grid-container">
+            <div className="grid-item">1</div>
+            <div className="grid-item">2</div>
+            <div className="grid-item">3</div>
+            <div className="grid-item">4</div>
+            <div className="grid-item">5</div>
+            <div className="grid-item">6</div>
+            <div className="grid-item">7</div>
+            <div className="grid-item">8</div>
+            <div className="grid-item">9</div>
+          </div>
+        </div>
       </div>
     )
   }
