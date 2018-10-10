f4ea4947e9fc0ab6f9d050dbd6ab474ec92ee02d
Brandon Courtney <brandon.h.courtney@gmail.com>
2018-10-09 22:50:36 -0700

    Adds Redux Dev tools

--- client/App.js
+++ client/App.js
@@ -7,7 +7,9 @@ import Home from './src/Home/index.js';
 import { Provider } from 'react-redux';
 import RootReducer from '../client/redux/reducers/baseReducer.js';
 
-var initStore = createStore(RootReducer);
+/* eslint-disable no-underscore-dangle */
+var initStore = createStore(RootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+/* eslint-enable */
 
 const App = () => (
   <Provider store={initStore}>
@@ -36,46 +38,3 @@ const App = () => (
 );
 
 export default App;
-
-// import React from "react";
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-// import Babel from "./src/Babel/index.js";
-// import Topics from "./src/Topics/index.js";
-
-// const App = () => (
-//   <Router>
-//     <div>
-//       <ul>
-//         <li>
-//           <Link to="/">Home</Link>
-//         </li>
-//         <li>
-//           <Link to="/babel">Babel</Link>
-//         </li>
-//         <li>
-//           <Link to="/topics">Subrouting example</Link>
-//         </li>
-//       </ul>
-
-//       <hr />
-
-//       <Route exact path="/" component={Home} />
-//       <Route path="/babel" component={Babel} />
-//       <Route path="/topics" component={Topics} />
-//     </div>
-//   </Router>
-// );
-
-// const Home = () => (
-//   <div>
-//     <h2>Home</h2>
-//   </div>
-// );
-
-// const About = () => (
-//   <div>
-//     <h2>About</h2>
-//   </div>
-// );
-
-// export default App;
